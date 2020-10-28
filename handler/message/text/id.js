22c75f535b820f8eee1fc380001ace6ad38e0cdf
Evil Code <57234832+aditalf@users.noreply.github.com>
2020-10-28 16:34:50 +0700

    Update id.js

--- handler/message/text/id.js
+++ handler/message/text/id.js
@@ -109,8 +109,8 @@ Untuk menghapus pesan bot (balas pesan bot dengan #del)`
 exports.textDonasi = () => {
     return `
 Hai, terimakasih telah menggunakan bot ini, untuk mendukung bot ini kamu dapat membantu dengan berdonasi melalui link berikut:
-1. Saweria: https://saweria.co/yogasakti
-2. Trakteer: https://trakteer.id/red-emperor 
+1. Saweria: https://saweria.co/aditiaalfian
+2. Trakteer: https://trakteer.id/aditiaalfiansyah 
 
 Donasi akan digunakan untuk pengembangan dan pengoperasian bot ini.
 
